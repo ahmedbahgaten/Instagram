df92e6aa01a6bfcb1ec3b414ad4c17605c30bf6e
ahmedbahgaten <ahmedbahgatnabih@gmail.com>
2020-01-13 05:32:59 -0800

    setting up the TabBar icon test and navigationBar title

---
+++ Instagram/MainTabBarController.swift
@@ -0,0 +1,23 @@
+//
+//  MainTabBarController.swift
+//  Instagram
+//
+//  Created by AHMED on 1/13/20.
+//  Copyright © 2020 AHMED. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let layout = UICollectionViewLayout()
+        let userProfileController = UserProfileController(collectionViewLayout: layout)
+        let navController = UINavigationController(rootViewController: userProfileController)
+        navController.tabBarItem.selectedImage = #imageLiteral(resourceName: "profile_selected")
+        navController.tabBarItem.image = #imageLiteral(resourceName: "profile_unselected")
+        tabBar.tintColor = .black
+        viewControllers = [navController,UIViewController()]
+    }
+}
